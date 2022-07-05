53a62022499f4d8d32c10449ad6f7417332ff47d
Oscar Bolanos <100238719+ob-instaleap@users.noreply.github.com>
2022-07-05 08:48:22 -0500

    LaboratoryEDX
    LaboratoryEDX

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
 import React from 'react';
-
+const axios = require('axios').default;
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  state = { counter:0 }
+  state = { eventList: [] }
 
-  incrementCounter = ()=> {
-    this.setState({counter:this.state.counter+1});
+  componentDidMount() {
+    let url = "https://reststop.randomhouse.com/resources/authorevents/?start=0&max=3&expandLevel=1&isbn=0&authorid=0"
+    const req = axios.get(url);
+    console.log(req);
+    req.then(resp => {
+      let listOfEvents = resp.data.authorEvent;
+      let listOfEventsAsArray = Object.entries(listOfEvents);
+      let eventDetails = listOfEventsAsArray.map((eventDetial) => {
+        let eventListCollection = Object.entries(eventDetial[1])
+        return <div>{eventListCollection[4][1]} on {eventListCollection[5][1]} at {eventListCollection[7][1]} in {eventListCollection[10][1]} - {eventListCollection[11][1]}</div>
+      })
+      this.setState({ eventList: eventDetails })
+    })
+      .catch(err => {
+        console.log(err.toString())
+      });
   }
 
   render() {
-    const colorStyle = { color:this.props.color,fontSize:this.props.size+"px"}
+    const colorStyle = { color: this.props.color, fontSize: this.props.size + "px" }
+    let li_ctr = 0;
     return (
       <div style={colorStyle}>
-        iTeknologyCounter for stupids!
-        <br /><br />
-        <button onClick={this.incrementCounter}>Click Me!</button>
-        <br /><br />
-        {this.state.counter}
+        React Component
+        <ul>
+          {this.state.eventList.map(function (eventDetails) {
+            return <li key={li_ctr++}>{eventDetails}</li>;
+          })}
+        </ul>
       </div>
     );
   }
